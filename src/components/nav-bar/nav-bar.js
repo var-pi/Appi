3011368c5761a840b625c87bcb061979e685d3ed
var-pi <stefanehin4@gmail.com>
2022-10-25 12:05:19 +0300

    refactor

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,31 +1,58 @@
-import { pages } from "../../router/routes.js";
+// class NavBar extends HTMLElement {
+//   constructor() {
+//     super();
+//   }
 
-class NavBar extends HTMLElement {
+//   connectedCallback() {
+//     let aTagList = "";
+//     for (const name in pages) {
+//       aTagList += `<a href="/#${pages[name].route}">${name}</a>`;
+//     }
+
+//     this.innerHTML = `
+//         <style>
+//             #container {
+//                 position: fixed;
+//                 top: 0px;
+//                 display: flex;
+//                 justify-content: center;
+//                 width: 100vw;
+//             }
+//         </style>
+//         <div id="container">
+//           ${aTagList}
+//         </div>
+//         `;
+//   }
+// }
+// customElements.define("nav-bar", NavBar);
+
+import { pages } from "../../router/routes.js";
+import CustomElement from "/src/components/customElement.js";
+
+class NavBar extends CustomElement {
   constructor() {
-    super();
+    super("/nav-bar", "nav-bar");
   }
 
-  connectedCallback() {
-    let aTagList = "";
-    for (const name in pages) {
-      aTagList += `<a href="/#${pages[name].route}">${name}</a>`;
-    }
+  async connectedCallback() {
+    await super.connectedCallback();
 
-    this.innerHTML = `
-        <style>
-            #container {
-                position: fixed;
-                top: 0px;
-                display: flex;
-                justify-content: center;
-                width: 100vw;
-            }
-        </style>
-        <div id="container">            
-          ${aTagList}
-        </div>
-        `;
+    // let aTagList = "";
+    // for (const name in pages) {
+    //   aTagList += `<a href="/#${pages[name].route}">${name}</a>`;
+    // }
+
+    // setTimeout(() => {
+    //   console.log(this.innerHTML);
+    // }, 300);
+    // setTimeout(() => {
+    //   const containerDiv = document.getElementById("container");
+    //   console.warn("containerDiv", containerDiv);
+    //   containerDiv.innerHTML = aTagList;
+    // }, 300);
   }
 }
+
 customElements.define("nav-bar", NavBar);
 
